9877a59a5eaa7bd4aba5552b39fb4c849afee1c0
David Rodney <davidrodney@interativaonline.com>
2018-09-28 16:06:12 -0300

    David

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.calculaVotacao([1,2,3,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5],true);
     this.sunvoteService.getVotes().subscribe((votos)=>{
-      console.log(this.xmlToJson(votos));
+      // console.log(this.xmlToJson(votos));
     })
 
   }
@@ -53,42 +53,42 @@ export class AppComponent implements OnInit {
     this.falsoSize = this.falsoAmount + '%';
     this.verdadeiroSize = this.verdadeiroAmount + '%';
   }  ;
-  xmlToJson(xml) {
+  // xmlToJson(xml) {
 	
-    // Create the return object
-    var obj = {};
+    // // Create the return object
+    // var obj = {};
   
-    if (xml.nodeType == 1) { // element
-      // do attributes
-      if (xml.attributes.length > 0) {
-      obj["@attributes"] = {};
-        for (var j = 0; j < xml.attributes.length; j++) {
-          var attribute = xml.attributes.item(j);
-          obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-        }
-      }
-    } else if (xml.nodeType == 3) { // text
-      obj = xml.nodeValue;
-    }
+    // if (xml.nodeType == 1) { // element
+      // // do attributes
+      // if (xml.attributes.length > 0) {
+      // obj["@attributes"] = {};
+        // for (var j = 0; j < xml.attributes.length; j++) {
+          // var attribute = xml.attributes.item(j);
+          // obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+        // }
+      // }
+    // } else if (xml.nodeType == 3) { // text
+      // obj = xml.nodeValue;
+    // }
   
-    // do children
-    if (xml.hasChildNodes()) {
-      for(var i = 0; i < xml.childNodes.length; i++) {
-        var item = xml.childNodes.item(i);
-        var nodeName = item.nodeName;
-        if (typeof(obj[nodeName]) == "undefined") {
-          obj[nodeName] = xmlToJson(item);
-        } else {
-          if (typeof(obj[nodeName].push) == "undefined") {
-            var old = obj[nodeName];
-            obj[nodeName] = [];
-            obj[nodeName].push(old);
-          }
-          obj[nodeName].push(xmlToJson(item));
-        }
-      }
-    }
-    return obj;
-  };
+    // // do children
+    // if (xml.hasChildNodes()) {
+      // for(var i = 0; i < xml.childNodes.length; i++) {
+        // var item = xml.childNodes.item(i);
+        // var nodeName = item.nodeName;
+        // if (typeof(obj[nodeName]) == "undefined") {
+          // obj[nodeName] = xmlToJson(item);
+        // } else {
+          // if (typeof(obj[nodeName].push) == "undefined") {
+            // var old = obj[nodeName];
+            // obj[nodeName] = [];
+            // obj[nodeName].push(old);
+          // }
+          // obj[nodeName].push(xmlToJson(item));
+        // }
+      // }
+    // }
+    // return obj;
+  // };
   title = 'sicoob';
 }
